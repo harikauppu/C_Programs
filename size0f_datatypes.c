9c44decf02079f878f1516d17b00ec78ee85a1ee
HARIKA.UPPU <uppuharikahan123@gmail.com>
2026-06-27 15:21:59 +0530

    Implemented feature

---
+++ size0f_datatypes.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main()
+{
+    printf("sizeof(char):%zu", sizeof(char));
+}
---
+++ sizeof_datatypes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf("sizeof char :%zu\n", sizeof(char));
+    printf("sizeof int :%zu\n",sizeof(int));
+    printf("sizeof double :%zu\n",sizeof(double));
+    printf("size of float :%zu\n",sizeof(float));
+    printf("sizeof short :%zu\n", sizeof(short));
+    printf("sizeof long :%zu\n", sizeof(long));
+    printf("sizeof long long :%zu\n", sizeof(long long));
+    printf("sizeof long double :%zu\n", sizeof(long double));
+}
